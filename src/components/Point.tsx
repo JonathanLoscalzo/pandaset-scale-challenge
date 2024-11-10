aebe3eff01cdea25883c74d2464eaa3f89578480
Jonathan Loscalzo <loscalzo.jony@gmail.com>
2024-11-09 21:04:13 -0300

    edit points to render at least MAX_POINTS
    Due to problems with resizing of buffers, we should limit this

--- src/components/Point.tsx
+++ src/components/Point.tsx
@@ -1,21 +1,31 @@
 import { Point as PointType } from '../types';
-import { Points as PointsDrei, PointMaterial } from '@react-three/drei';
+import { Points as PointsDrei } from '@react-three/drei';
 import { generateColor } from '../utils';
+import { useMemo } from 'react';
 
 export function Points(props: { points: PointType[] }) {
-  const positions = new Float32Array(props.points.length * 3);
-  const colors = new Float32Array(props.points.length * 3);
+  // Due to performances issues related to the resizing of a buffered array,
+  // we limit the number of points to render to be equals between re-renderings
+  const MAX_POINTS = 1000000;
+  const { positions, colors } = useMemo(() => {
+    const points = props.points.slice(0, MAX_POINTS);
 
-  props.points.forEach(([x, y, z], i) => {
-    positions.set([x, y, z], i * 3);
-    const color = generateColor(z);
-    colors.set([color.r, color.g, color.b], i * 3);
-  });
+    const positions = new Float32Array(points.length * 3);
+    const colors = new Float32Array(points.length * 3);
+
+    points.forEach(([x, y, z], i) => {
+      positions.set([x, y, z], i * 3);
+      const color = generateColor(z);
+      colors.set([color.r, color.g, color.b], i * 3);
+    });
+
+    return { positions, colors };
+  }, [props.points]);
 
   return (
     <>
-      <PointsDrei positions={positions} colors={colors} frustumCulled={false}>
-        <PointMaterial vertexColors size={0.1} />
+      <PointsDrei colors={colors} positions={positions} sizes={new Float32Array(MAX_POINTS * 3)}>
+        <pointsMaterial transparent vertexColors size={0.1} />
       </PointsDrei>
     </>
   );
